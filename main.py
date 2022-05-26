11806d25cea1ee0c28124322d6b9d8dff1054a92
zDudaHang <maria.eduarda.melo.hang@gmail.com>
2022-05-26 09:44:24 -0300

    Ajustes no código

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
 import json
 import os
 
+from src.constants.paths import *
+from src.constants.portal_sp import *
+
 from bs4 import BeautifulSoup
-
-
-BASE_SP_FILE_DIR = 'resources/sp/'
-JOB_1 = 'secretaria_dos_esportes/'
-FILE_EXTENSION = '.html'
-RESULT_FOLDER = 'results/'
-
-NOME = 'NOME'
-ORGAO = 'ORGAO'
-CARGO = 'CARGO'
-REMUNERACAO_MES = 'REMUNERACAO_DO_MES'
-FERIAS_E_13_SALARIO = 'FERIAS_E_13_SALARIO'
-PAGAMENTOS_EVENTUAIS = 'PAGAMENTOS_EVENTUAIS'
-LICENCA_PREMIO_INDENIZADA = 'LICENCA_PREMIO_INDENIZADA'
-ABONO_PERMANENCIA_E_OUTRAS_INDENIZACOES = 'ABONO_PERMANENCIA_E_OUTRAS_INDENIZACOES'
-REDUTOR_SALARIAL = 'REDUTOR_SALARIAL'
-TOTAL_LIQUIDO = 'TOTAL_LIQUIDO'
-NOME_CAMPOS = [NOME, ORGAO, CARGO, REMUNERACAO_MES, FERIAS_E_13_SALARIO, PAGAMENTOS_EVENTUAIS, LICENCA_PREMIO_INDENIZADA, ABONO_PERMANENCIA_E_OUTRAS_INDENIZACOES, REDUTOR_SALARIAL, TOTAL_LIQUIDO]
 
 # https://stackabuse.com/guide-to-parsing-html-with-beautifulsoup-in-python/ <<<<<====== link de guia para uso da ferramenta
 
 if __name__ == '__main__':
     infos_salariais = dict()
     n_entradas = 1;
-
+    # TODO: Transformar isso em uma classe para o código ficar mais limpo
+    path_to_sp_job1 = os.path.join(RESOURCES_DIR, SP_RESOURCES_DIR, JOB_1)
     # itera pelas paginas salvas em um diretorio qualquer
-    for page in range (1, len(os.listdir(BASE_SP_FILE_DIR+JOB_1)) + 1):
+    for page in range (1, len(os.listdir(path_to_sp_job1)) + 1):
        # abre uma pagina qualquer
-       with open(BASE_SP_FILE_DIR + JOB_1 + str(page) + FILE_EXTENSION, 'r') as fp:
+       path_to_page = os.path.join(path_to_sp_job1, str(page) + FILE_EXTENSION)
+       with open(path_to_page, 'r') as fp:
             soup = BeautifulSoup(fp, 'html.parser')
             # acha a tag com as informacoes desejadas
             result = soup.find('table', attrs={'rules': 'all'})
@@ -55,7 +42,11 @@ if __name__ == '__main__':
                 print(i, '->', info)
 
     # escreve os dados serializados para um arquivo qualquer
-    filename = BASE_SP_FILE_DIR + RESULT_FOLDER + JOB_1 + 'data.json'
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, 'w') as outfile:
+    result_path = os.path.join(RESOURCES_DIR, SP_RESOURCES_DIR, RESULT_FOLDER, JOB_1, RESULT_FILENAME)
+    os.makedirs(os.path.dirname(result_path), exist_ok=True)
+    with open(result_path, 'w') as outfile:
         json.dump(infos_salariais, outfile)
+
+
+def extrair_portal_transparencia_sc():
+    pass
