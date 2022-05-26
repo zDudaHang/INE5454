660f910b1fc506d4fa03c2859a0e9424f2e3d1ca
Arthur Moreira Rodrigues Alves <artmr.alves@gmail.com>
2022-05-25 22:22:16 -0300

    Update code to extraxt data from multiple pages.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import json
+import os
+
 from bs4 import BeautifulSoup
+
+
+BASE_SP_FILE_DIR = 'resources/sp/'
+JOB_1 = 'secretaria_dos_esportes/'
+FILE_EXTENSION = '.html'
+RESULT_FOLDER = 'result/'
 
 NOME = 'NOME'
 ORGAO = 'ORGAO'
@@ -15,19 +24,38 @@ NOME_CAMPOS = [NOME, ORGAO, CARGO, REMUNERACAO_MES, FERIAS_E_13_SALARIO, PAGAMEN
 # https://stackabuse.com/guide-to-parsing-html-with-beautifulsoup-in-python/ <<<<<====== link de guia para uso da ferramenta
 
 if __name__ == '__main__':
-    with open('resources/sp/prof_1/Remuneracao.html', 'r') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
-        result = soup.find('table', attrs={'rules': 'all'})
-        trs = result.find_all('tr', attrs=None)
-        infos_salariais = dict()
-        n_entradas = 0;
-        for tr in trs:
-            tds = tr.find_all('td')
-            if len(tds) < 1:
-                continue
-            line = dict()
-            for i in range(10):
-                line.update({NOME_CAMPOS[i]: tds[i].text})
-            infos_salariais.update({n_entradas: line})
-            n_entradas = n_entradas + 1
-        print('bk')
+    infos_salariais = dict()
+    n_entradas = 1;
+
+    # itera pelas paginas salvas em um diretorio qualquer
+    for page in range (1, len(os.listdir(BASE_SP_FILE_DIR+JOB_1)) + 1):
+       # abre uma pagina qualquer
+       with open(BASE_SP_FILE_DIR + JOB_1 + str(page) + FILE_EXTENSION, 'r') as fp:
+            soup = BeautifulSoup(fp, 'html.parser')
+            # acha a tag com as informacoes desejadas
+            result = soup.find('table', attrs={'rules': 'all'})
+            # obtem o elemento de linha de tabela contida na tabela desejada
+            trs = result.find_all('tr', attrs=None)
+            # itera sobre os elementos linha
+            for tr in trs:
+                tds = tr.find_all('td')
+                # checa se uma linha contem apenas infos de cabecalho; se sim vai para proxima
+                if len(tds) < 1:
+                    continue
+                line = dict()
+                # cria um dicionario com as informacoes de uma linha
+                for i in range(10):
+                    line.update({NOME_CAMPOS[i]: tds[i].text})
+                # atualiza o dicionario principal
+                infos_salariais.update({n_entradas: line})
+                n_entradas = n_entradas + 1
+
+            # apenas imprime o dicionario obtido
+            for i, info in infos_salariais.items():
+                print(i, '->', info)
+
+    # escreve os dados serializados para um arquivo qualquer
+    filename = BASE_SP_FILE_DIR + JOB_1 + RESULT_FOLDER + 'data.json'
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w') as outfile:
+        json.dump(infos_salariais, outfile)
