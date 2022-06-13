11a671a87e844fcf067796f8446f5ce7b9cf274e
Arthur Moreira Rodrigues Alves <artmr.alves@gmail.com>
2022-06-13 11:02:16 -0300

    Salvando trabalho.

--- src/requester/pr_requester.py
+++ src/requester/pr_requester.py
@@ -4,19 +4,66 @@ from src.requester.requester import Requester
 
 
 class PrRequester(Requester):
-    def __init__(self):
-        self.page = ''
+    URL = 'http://www.transparencia.pr.gov.br/pte/pessoal/servidores/poderexecutivo/remuneracao?windowId=3fc'
 
-    def get_html(self) -> None:
-        params = dict()
-        r = requests.get(self.URL, params=params)
-        pass
-
-    def get_page(self) -> str:
-        pass
-
-    def has_next(self) -> bool:
-        pass
+    def __init__(self, instituicao: str = 'TODAS', cargo: str = 'TODOS', municipio: str = 'TODOS', quadro_funcional: str = 'TODOS'):
+        self.instituicao = instituicao
+        self.cargo = cargo
+        self.municipio = municipio
+        self.quadro_funcional = quadro_funcional
+        self.ViewState = '-2440845790774114559:1958432107984863730'
+        self.status_code = '200'
 
     def get_next(self) -> None:
-        pass
+        self.get_html()
+        # qualquer tratamento especifico
+
+    def get_html(self) -> None:
+        data = {
+            'javax.faces.partial.ajax': 'true',
+            'formRemuneracoes': 'formRemuneracoes',
+            'formRemuneracoes:filtroNome': '',
+            'formRemuneracoes:filtroInstituicao_focus': '',
+            'formRemuneracoes:filtroInstituicao_input': '',
+            'formRemuneracoes:filtroInstituicao_editableInput': self.instituicao,
+            'formRemuneracoes:filtroCargo_focus': '',
+            'formRemuneracoes:filtroCargo_input': '',
+            'formRemuneracoes:filtroCargo_editableInput': self.cargo,
+            'formRemuneracoes:filtroMunicipio_focus': '',
+            'formRemuneracoes:filtroMunicipio_input': '',
+            'formRemuneracoes:filtroMunicipio_editableInput': self.municipio,
+            'formRemuneracoes:filtroQuadroFuncional_focus': '',
+            'formRemuneracoes:filtroQuadroFuncional_input': '',
+            'formRemuneracoes:filtroQuadroFuncional_editableInput': self.quadro_funcional,
+            'formRemuneracoes:customRadio': 'false',
+            'javax.faces.ViewState': self.ViewState
+        }
+        if not self.STARTED:
+            data.update(
+                {
+                    'javax.faces.source': 'formRemuneracoes:buttonPesquisar',
+                    'javax.faces.partial.execute': 'formRemuneracoes',
+                    'javax.faces.partial.render': 'formRemuneracoes',
+                    'formRemuneracoes:buttonPesquisar': 'formRemuneracoes:buttonPesquisar',
+                }
+            )
+        else:
+            data.update(
+                {
+                    'javax.faces.source': 'formRemuneracoes:dataTableServidores',
+                    'javax.faces.partial.execute': 'formRemuneracoes:dataTableServidores',
+                    'javax.faces.partial.render': 'formRemuneracoes:dataTableServidores',
+                    'javax.faces.behavior.event': 'page',
+                    'javax.faces.partial.event': 'page',
+                    'formRemuneracoes:dataTableServidores_pagination': 'true',
+                    'formRemuneracoes:dataTableServidores_first': '30',
+                    'formRemuneracoes:dataTableServidores_rows': '30',
+                    'formRemuneracoes:dataTableServidores_encodeFeature': 'true'
+                }
+            )
+        r = requests.post(self.URL, data=data, headers=self.HEADERS)
+        self.PAGE = r.text
+        self.status_code = r.status_code
+
+    def has_next(self) -> bool:
+        return self.status_code == 200
--- src/requester/requester.py
+++ src/requester/requester.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 
 
 class Requester(ABC):
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0'}
+    PAGE = ''
 
     @abstractmethod
     def get_next(self) -> None:
@@ -11,9 +13,9 @@ class Requester(ABC):
     def get_html(self) -> None:
         pass
 
-    @abstractmethod
     def get_page(self) -> str:
-        pass
+        self.get_next()
+        return self.PAGE
 
     @abstractmethod
     def has_next(self) -> bool:
