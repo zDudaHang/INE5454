af4cd93d1fcdb4a49d3aaf29496a45a203569994
Arthur Moreira Rodrigues Alves <Arthur Moreira Rodrigues Alves>
2022-07-06 11:06:08 -0300

    Atualizando código do requester de pr; só falta ele conseguir pegar as coisas da proxima pagina

--- src/requester/pr_requester.py
+++ src/requester/pr_requester.py
@@ -1,24 +1,36 @@
+import re
+
 import requests
 
 from src.requester.requester import Requester
 
 
 class PrRequester(Requester):
-    URL = 'http://www.transparencia.pr.gov.br/pte/pessoal/servidores/poderexecutivo/remuneracao?windowId=3fc'
+    URL = 'http://www.transparencia.pr.gov.br/pte/pessoal/servidores/poderexecutivo/remuneracao'
 
     def __init__(self, instituicao: str = 'TODAS', cargo: str = 'TODOS', municipio: str = 'TODOS', quadro_funcional: str = 'TODOS'):
         self.instituicao = instituicao
         self.cargo = cargo
         self.municipio = municipio
         self.quadro_funcional = quadro_funcional
-        self.ViewState = '-2440845790774114559:1958432107984863730'
         self.status_code = '200'
+        self.dataTableServidores_first = 30
+        self.window_id = ''
+        self.jsessionid = ''
 
-    def get_next(self) -> None:
+    def get_next(self):
         self.get_html()
-        # qualquer tratamento especifico
+        # ATUALIZAR VIEWSATE
+        self.VIEWSTATE = re.search(r'ViewState:0"><!\[CDATA\[(.*?)]]>', self.PAGE).group(1)
+        # ATUALIZAR JSESSION
+        # ATUALIZAR WINDOW_ID
+        if not self.STARTED:
+            self.jsessionid = re.search(r';jsessionid=(.*?)\?', self.PAGE).group(1)
+            self.window_id = re.search(r'\?windowId=(.*?)"', self.PAGE).group(1)
+        return self.PAGE
 
     def get_html(self) -> None:
+        cookies = dict()
         data = {
             'javax.faces.partial.ajax': 'true',
             'formRemuneracoes': 'formRemuneracoes',
@@ -35,19 +47,10 @@ class PrRequester(Requester):
             'formRemuneracoes:filtroQuadroFuncional_focus': '',
             'formRemuneracoes:filtroQuadroFuncional_input': '',
             'formRemuneracoes:filtroQuadroFuncional_editableInput': self.quadro_funcional,
-            'formRemuneracoes:customRadio': 'false',
-            'javax.faces.ViewState': self.ViewState
+            'formRemuneracoes:customRadio': 'false'
         }
-        if not self.STARTED:
-            data.update(
-                {
-                    'javax.faces.source': 'formRemuneracoes:buttonPesquisar',
-                    'javax.faces.partial.execute': 'formRemuneracoes',
-                    'javax.faces.partial.render': 'formRemuneracoes',
-                    'formRemuneracoes:buttonPesquisar': 'formRemuneracoes:buttonPesquisar',
-                }
-            )
-        else:
+        url = ''
+        if self.STARTED:
             data.update(
                 {
                     'javax.faces.source': 'formRemuneracoes:dataTableServidores',
@@ -56,13 +59,35 @@ class PrRequester(Requester):
                     'javax.faces.behavior.event': 'page',
                     'javax.faces.partial.event': 'page',
                     'formRemuneracoes:dataTableServidores_pagination': 'true',
-                    'formRemuneracoes:dataTableServidores_first': '30',
+                    'formRemuneracoes:dataTableServidores_first': str(self.dataTableServidores_first),
                     'formRemuneracoes:dataTableServidores_rows': '30',
-                    'formRemuneracoes:dataTableServidores_encodeFeature': 'true'
+                    'formRemuneracoes:dataTableServidores_encodeFeature': 'true',
+                    'javax.faces.ViewState': self.VIEWSTATE
+
                 }
             )
-        r = requests.post(self.URL, data=data, headers=self.HEADERS)
-        self.PAGE = r.text
+            cookies.update(
+                {
+                    'JSESSIONID': self.jsessionid
+                }
+            )
+            self.dataTableServidores_first = self.dataTableServidores_first + 30
+            url = self.URL + '?windowId=' + self.window_id
+        else:
+            data.update(
+                {
+                    'javax.faces.source': 'formRemuneracoes:buttonPesquisar',
+                    'javax.faces.partial.execute': 'formRemuneracoes',
+                    'javax.faces.partial.render': 'formRemuneracoes',
+                    'formRemuneracoes:buttonPesquisar': 'formRemuneracoes:buttonPesquisar',
+                }
+            )
+            self.STARTED = True
+            url = self.URL
+
+        r = requests.post(url, data=data, headers=self.HEADERS, cookies=cookies)
+        PAGE = r.text
+        self.PAGE = PAGE
         self.status_code = r.status_code
 
     def has_next(self) -> bool:
--- src/requester/requester.py
+++ src/requester/requester.py
@@ -2,11 +2,14 @@ from abc import ABC, abstractmethod
 
 
 class Requester(ABC):
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0'}
-    PAGE = ''
+    URL: str = ''
+    HEADERS: dict = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0'}
+    PAGE: str = ''
+    VIEWSTATE: str = ''
+    STARTED: bool = False
 
     @abstractmethod
-    def get_next(self) -> None:
+    def get_next(self):
         pass
 
     @abstractmethod
